0722959daca0a4f356f1e3c93b7d252ea8b0c793
roshankhapre <roshankhapre30@gmail.com>
2025-09-18 17:51:58 +0530

    Connection frontend problem solve

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -1,10 +1,3 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
-  withCredentials: true, // ✅ allow cookies
-});
-
 import API from "./axios";
 
 // Admin-specific APIs
