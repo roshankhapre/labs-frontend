b510c25af80c5642dbe07f5e16e64e901f593c23
roshankhapre <roshankhapre30@gmail.com>
2025-09-18 16:22:36 +0530

    Connection Backend And Frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // 👈 single source of truth
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
   withCredentials: false,
 });
 
